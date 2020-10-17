02509adffaff8176ac413b4c2faefb630e4e1b68
Silver Liivamägi <silver.liivamagi@gmail.com>
2020-10-17 16:31:16 +0300

    fixes for heroku

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,7 @@ const BG_COLOUR = '#231f20';
 const SNAKE_COLOUR = "#c2c2c2";
 const FOOD_COLOUR = "#e66916";
 
-const socket = io('https://damp-sierra-00472.herokuapp.com/');
+const socket = io('https://stark-reef-95519.herokuapp.com/');
 
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
